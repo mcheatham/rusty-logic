d72b5327c211671ba987b25621a61842b35f5413
Demon_Munchies <hirtquinn@gmail.com>
2020-06-25 21:31:11 -0400

    modularized enum instance creation, added unary node, added print for tree

--- src/enum_ast.rs
+++ src/enum_ast.rs
@@ -1,6 +1,7 @@
 pub enum Node
 {
-	InnerNode(Box<Node>, Box<Node>,  Box<dyn Fn(bool, bool) -> bool>),
+	BinaryInnerNode(Box<Node>, Box<Node>,  Box<dyn Fn(bool, bool) -> bool>),
+	UnaryInnerNode(Box<Node>, Box<dyn Fn(bool) -> bool>),
 	LeafNode(bool),
 }
 
@@ -8,25 +9,68 @@ pub fn eval(node: Node) -> bool
 {
 	let val = match node
 	{
-		Node::InnerNode(a, b, op) =>
+		Node::BinaryInnerNode(a, b, op) =>
 		{
 			let _nested_a = match *a
 			{
-				Node::InnerNode(n_a, n_b, n_op) => n_op(eval(*n_a), eval(*n_b)),
+				Node::BinaryInnerNode(n_a, n_b, n_op) => n_op(eval(*n_a), eval(*n_b)),
+				Node::UnaryInnerNode(b, n_op) => n_op(eval(*b)),
 				Node::LeafNode(b) => b,
 			};
 
 			let _nested_b = match *b
 			{
-				Node::InnerNode(n_a, n_b, n_op) => n_op(eval(*n_a), eval(*n_b)),
+				Node::BinaryInnerNode(n_a, n_b, n_op) => n_op(eval(*n_a), eval(*n_b)),
+				Node::UnaryInnerNode(b, n_op) => n_op(eval(*b)),
 				Node::LeafNode(b) => b,
 			};
 
 			op(_nested_a, _nested_b)
 			
 		},
+		Node::UnaryInnerNode(b, op) => op(eval(*b)),
 		Node::LeafNode(b) => b
 	};
 
 	val
+}
+
+pub fn print_expression(node: Node) -> String
+{
+	let mut s: String = String::from("");
+
+	match node
+	{
+		Node::BinaryInnerNode(a, b, _op) => 
+		{
+			s.push_str(format!("{}{}{}{}{}", "[ ", print_expression(*a).as_str(), " OP ", print_expression(*b).as_str(), " ]").as_str());
+		},
+		Node::UnaryInnerNode(b, _op) =>
+		{
+			s.push_str(format!("{}{}{}{}", "[ ", " OP ", print_expression(*b).as_str(), " ]").as_str());
+		},
+		Node::LeafNode(b) =>
+		{
+			s.push_str(format!("{}", b).as_str());
+		},
+	}
+	s
+}
+
+pub fn make_binary_inner_node(left_node: Node, right_node: Node, op: Box<dyn Fn(bool, bool) -> bool>) -> Node
+{
+	let node = Node::BinaryInnerNode(Box::new(left_node), Box::new(right_node), op);
+	node
+}
+
+pub fn make_unary_inner_node(child_node: Node, op: Box<dyn Fn(bool) -> bool>) -> Node
+{
+	let node = Node::UnaryInnerNode(Box::new(child_node), op);
+	node
+}
+
+pub fn make_leaf_node(val: bool) -> Node
+{
+	let node = Node::LeafNode(val);
+	node
 }
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,25 @@ mod enum_ast;
 use ast::Node;
 
 fn main() {
+	
     let mut tree_root = Node{val:"and", l:None, r:None};
     tree_root.set_left("A");
     tree_root.set_right("B");
     tree_root.print();
 
-    let a: Box<enum_ast::Node> = Box::new(enum_ast::Node::LeafNode(true));
-    let b: Box<enum_ast::Node> = Box::new(enum_ast::Node::LeafNode(false));
-    let my_fn: Box<dyn Fn(bool, bool) -> bool> = Box::new(|a, b| a || b);
+    let leaf_1: enum_ast::Node = enum_ast::make_leaf_node(true);
+    let leaf_2: enum_ast::Node = enum_ast::make_leaf_node(false);
+    let leaf_3: enum_ast::Node = enum_ast::make_leaf_node(true);
+    let leaf_4: enum_ast::Node = enum_ast::make_leaf_node(false);
 
-    let inner_node = enum_ast::Node::InnerNode(a, b, my_fn);
+    let inner_1_2: enum_ast::Node = enum_ast::make_binary_inner_node(leaf_1, leaf_2, Box::new(|a, b| a || b));
+    let inner_3_4: enum_ast::Node = enum_ast::make_binary_inner_node(leaf_3, leaf_4, Box::new(|a, b| a && b));
 
-    let inner_ret = enum_ast::eval(inner_node);
-    println!("{}", inner_ret);
+    let inner_12_34: enum_ast::Node = enum_ast::make_binary_inner_node(inner_1_2, inner_3_4, Box::new(|a, b| a && b));
+    let unary_last: enum_ast::Node = enum_ast::make_unary_inner_node(inner_12_34, Box::new(|a| !a));
+
+    // let bool_ret = enum_ast::eval(unary_last);
+    // println!("{}", bool_ret);
+
+    println!("{}", enum_ast::print_expression(unary_last));
 }
